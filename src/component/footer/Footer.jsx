28d98c572203d38866175c7fde084591acb757f4
Mokarama <mokaramaaktershanta@gmail.com>
2025-10-21 21:08:59 +0600

    Footer added

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,16 +1,30 @@
 "use client"
 
 import React from 'react'
+import styles from './page.module.css'
+import Image from "next/image";
 
 export default function Footer
 () {
     // console.log("hello")
   return (
     <>
-        <div>
+        <div className={styles.container}>
             <div>2025 Lamamia. All rights reserved.</div>
-            <div>
+            <div className={styles.social}>
+              
+                 <Image  className={styles.icon} src="/facebook.png" width={20} height={20}  alt="facebook"/>
+             
+                 
+               
+                  {/* <Image className={styles.icon}  src="/github.png" width={20} height={20} alt="github"/> */}
+                
+                    {/* <Image  className={styles.icon} src="/instagram.png" width={20} height={20} alt="instagram"/> */}
+                 
 
+                  
+                    <Image className={styles.icon} src="/linkedin.png"  width={20} height={20} alt="Lama Dev"/>
+                   
             </div>
         </div>
     </>
