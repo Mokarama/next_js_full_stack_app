2dc77d1b1143800f6e1897acec51542dec0c2357
Mokarama <mokaramaaktershanta@gmail.com>
2025-10-22 14:27:30 +0600

    add responsive design, set item backgrounds, and hover title effects

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,25 +1,33 @@
 import styles from "./page.module.css";
 
-
-import { styles } from '.';
 import Link from "next/link";
+
 export default function Portfolio() {
   return (
     <>
       <div className={styles.container}>
         <h1 className={styles.selectTitle}>Choose a gallery</h1>
         <div className={styles.items}>
-          <Link href="/portfolio/illustrations">
+          {/* Card-01 */}
+         <div className={styles.item}>
+           <Link href="/portfolio/illustrations">
           <span className={styles.title}>Illustrations</span>
           </Link>
+         </div>
 
-            <Link href="/portfolio/websites">
+          {/* card-02 */}
+           <div className={styles.item}>
+             <Link href="/portfolio/websites">
           <span className={styles.title}>Websites</span>
           </Link>
+           </div>
 
-            <Link href="/portfolio/application">
+           {/* card-03 */}
+          <div className={styles.item}>
+              <Link href="/portfolio/application">
           <span className={styles.title}>Application</span>
           </Link>
+          </div>
 
         </div>
       </div>
