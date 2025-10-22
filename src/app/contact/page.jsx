79f959c6bb37542d7af47727a0df5a7f855800a1
Mokarama <mokaramaaktershanta@gmail.com>
2025-10-22 13:41:48 +0600

    Create Portfolio component with links to Illustrations, Websites, and Applications

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,37 @@
+import Button from '../../component/Button/Button'
 import styles from './page.module.css'
+import Image from 'next/image'
 
 export default function Contact() {
   return (
-    <div className={styles.container}>Contact</div>
+    <>
+    <div className={styles.container}>
+    <h1 className={styles.title}>Let's Keep in Touch</h1>
+
+    <div className={styles.content}>
+        {/* Image */}
+       <div className={styles.imgContainer}>
+      <Image src="" className={styles.image} />
+       </div>
+
+   {/* //form container */}
+    <form className={styles.form}>
+      <input type="text" placeholder='name' className={styles.input} />
+      <input type="email" placeholder='email' className={styles.input} />
+      <textarea className={styles.textArea} placeholder='Message' cols="30" rows="10" ></textarea>
+      <Button url="#" text="Send"/>
+      
+    </form>
+
+    </div>
+
+   
+
+
+
+
+
+    </div>
+    </>
   )
 }
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,7 +1,28 @@
+import styles from "./page.module.css";
 
 
+import { styles } from '.';
+import Link from "next/link";
 export default function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <>
+      <div className={styles.container}>
+        <h1 className={styles.selectTitle}>Choose a gallery</h1>
+        <div className={styles.items}>
+          <Link href="/portfolio/illustrations">
+          <span className={styles.title}>Illustrations</span>
+          </Link>
+
+            <Link href="/portfolio/websites">
+          <span className={styles.title}>Websites</span>
+          </Link>
+
+            <Link href="/portfolio/application">
+          <span className={styles.title}>Application</span>
+          </Link>
+
+        </div>
+      </div>
+    </>
   )
 }
